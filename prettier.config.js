6e86aa9da75ee881e8bfd9425aff52e174555243
Varun Dev <varun.dev@hotmail.com>
2021-08-17 12:34:11 +0200

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid'
+}
