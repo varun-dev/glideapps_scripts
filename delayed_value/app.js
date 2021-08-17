d602b4e2cba178a93717c6eaa2ed440ee10ff8b8
Varun Dev <varun.dev@hotmail.com>
2021-08-17 14:38:00 +0200

    added interval

--- delayed_value/app.js
+++ delayed_value/app.js
@@ -3,20 +3,24 @@ window.addEventListener('message', async function (event) {
     origin,
     data: { key, params },
   } = event
-  console.log('Message', origin, key, params)
-  const [value, delay] = params
-  if (!value || !delay) {
-    respond('Missing params')
+  console.log('Params', params)
+  const [repeat, interval] = params
+  let ctr = 0
+  let timer
+  if (!repeat || !interval) {
+    respond(false)
   } else {
-    setTimeout(() => respond(value.value), delay.value)
+    timer = setInterval(() => respond(ctr%2), interval.value)
   }
 
   function respond(result) {
+    if(++ctr === repeat.value) clearInterval(timer)
     const response = { key }
     if (result !== undefined) {
-      response.result = { type: 'string', value: result }
+      response.result = { type: 'boolean', value: result }
     }
     event.source.postMessage(response, '*')
+    // console.log(result, '*')
   }
 })
 
